4d80718e0707bbc65f7dc11a0706ddc30a9606e2
Dikshant sharma <dikshantsharma874@gmail.com>
2022-05-18 16:38:50 +0530

    delete rows

--- sql-course-materials/SQL Course Materials/Insert and update'.sql
+++ sql-course-materials/SQL Course Materials/Insert and update'.sql
@@ -11,4 +11,28 @@ where order_date < "2019-01-01";
  
  update invoices 
  set payment_total=10,payment_date="2019-03-01"
- where invoice_id=1
+ where invoice_id=1;
+ 
+ update invoices
+ set 
+	payment_total=invoice_total * 0.5,
+    payment_date=due_date
+where invoice_id in (1,last_insert_id());
+
+--
+use sql_store;
+update customers
+set 
+	points=points+50
+where birth_date < "1990-01-01";
+
+update invoices
+set 
+	payment_total=invoice_total*0.5,
+    payment_date=due_date
+where client_id In
+		(select client_id 
+        from clients
+        where state in ("CA","NY"))
+
+
